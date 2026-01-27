0d07bd9029e3c445ae0760a3c890b2f0949940a3
Tobi <69075397+PepperPhil@users.noreply.github.com>
2026-01-27 09:46:23 +0100

    Change language to German and add PWA metadata
    Updated the RootLayout component to use German as the default language and added PWA metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,15 +9,18 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang='en' suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+    <html lang="de" suppressHydrationWarning>
+      <head>
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#111111" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      </head>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
