28bc98a1f3a42097a879db54d4e9a0b5818f4c8c
Tobi <69075397+PepperPhil@users.noreply.github.com>
2026-01-27 14:51:35 +0100

    Update ShareImportRunner and RecipeFetcher props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,24 @@ export default async function Page({
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const sp = await searchParams;
+
   const tagQuery = sp.tagQuery as string | undefined;
   const tags = (sp.tags as string)?.split(',').filter(Boolean) ?? [];
+
+  // Share-Target Params (/share redirectet auf /?url=...&autostart=1)
+  const sharedUrl = sp.url as string | undefined;
+  const autostart = sp.autostart === '1';
 
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-3xl font-bold'>Welcome to social to Mealie</h1>
 
-      {/* PWA Share-Import Runner: startet automatisch, wenn ?url=...&autostart=1 */}
-      <ShareImportRunner tags={tags} />
+      {/* startet automatisch, wenn autostart=1 */}
+      <ShareImportRunner tags={tags} sharedUrl={sharedUrl} autostart={autostart} />
 
-      <RecipeFetcher tags={tags} />
+      {/* zeigt die URL im Textfeld an */}
+      <RecipeFetcher tags={tags} sharedUrl={sharedUrl} />
+
       <div className='w-fit min-w-96 m-4'>
         <GetTagSelect query={tagQuery} />
       </div>
